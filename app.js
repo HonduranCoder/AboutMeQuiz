8512a6b7cfa1274e52b5925b8f64824c363a3478
Estefani <stef45689@gmail.com>
2021-08-16 17:07:33 -0500

    JS Started

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 // import functions and grab DOM elements
-
+import {countsAsAYes} from './utils.js'
 // initialize state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const button = document.getElementById('quizButton'); 
+const results = document.getElementById('quizResults');
+
+button.addEventListener('click', ()=>{
+  let rightAnswers = 0; 
+})
+
+const chosenName = prompt(Hi, please enter preferred name.);
+const continue = confirm(`Thanks, ${chosenName}, do you want to take the quiz?`)
+if(!continue) return;
+
+const answer1 = prompt('Is my favorite place New Zealand?');
+if (countsAsAYes(answer1) rightAnswers++;
+
+const answer2 = prompt('Does Mia dislike hiking?');
+if (countsAsAYes(answer2) rightAnswers++;
+
+const answer3 = prompt('Is Tegucicalpa the capital of Honduras');
+if (countsAsAYes(answer3) rightAnswers++;
+
+alert(`Thanks ${chosenName).`);
+results.textContent = `${chosenName} your results are: ${rightAnswers};
---
+++ utils.js
@@ -0,0 +1,3 @@
+export function countsAsAYes(someString){
+    return someString.charAt(0).toLowerCase() === 'y';
+}
