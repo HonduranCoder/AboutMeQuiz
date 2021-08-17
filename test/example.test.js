346733236b933a317bb75afd499cb46282942a14
Estefani <stef45689@gmail.com>
2021-08-16 19:35:31 -0500

    Test Edit

--- test/example.test.js
+++ test/example.test.js
@@ -6,7 +6,7 @@ const test = QUnit.test;
 
 test('Tell if the answer is yes or no', (expect) => {
     const expected = true;
-    const actual = countsAsAYes(yes);
+    const actual = countsAsAYes('yes');
 
     expect.equal(actual,expected, 'yes is true');
 });
